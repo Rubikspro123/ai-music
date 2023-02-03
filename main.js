dea63e969ea71f0cc383ceea51c7e211794ce85a
Rubikspro123 <87181104+Rubikspro123@users.noreply.github.com>
2023-02-03 18:17:22 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 harry_potter_music = '';
 super_mario_music = '';
-
+leftWristX = '';
+leftWristY = '';
+rightWristX = '';
+rightWristY = '';
 function preload(){
     harry_potter_music = loadSound('harrypotter.mp3');
     super_mario_music = loadSound('supermario.mp3');
@@ -12,8 +15,13 @@ function setup(){
     
     video = createCapture(VIDEO);
     video.hide();
-}
 
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
+}
+function modelLoaded(){
+    console.log('PoseNet is Initialized')
+}
 function draw(){
     image(video,0,0,600,500)
 }
@@ -26,4 +34,16 @@ function supersong(){
 function stop(){
     harry_potter_music.pause();
     super_mario_music.pause();
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log('leftWristX = ' + leftWristX + 'leftWristY = ' + leftWristY);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log('rightWristX = ' + rightWristX + 'rightWristY = ' + rightWristY);
+
+    }
 }
